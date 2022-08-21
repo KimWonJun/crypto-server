450f2d69437fa3a63b92fef074d995f94c7192ff
KimWonJun <kwj0313im@naver.com>
2022-08-21 18:53:54 +0900

    Test 파일 주석처리 - 2022.08.21 KimWonJun

--- src/test/java/com/my/cryptoserver/CryptoServerApplicationTests.java
+++ src/test/java/com/my/cryptoserver/CryptoServerApplicationTests.java
@@ -3,10 +3,10 @@ package com.my.cryptoserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CryptoServerApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
